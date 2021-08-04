d276a63b87dbfbfbf523d9eff9a825b7231a2a03
mitkousa <uk.dimitar@gmail.com>
2021-08-04 15:13:43 +0100

    route for all orders done

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import render_template
-from models.order_list import orders
+from models.order_list import *
 
 @app.route("/orders")
 def index():
     return render_template("index.html", orders=orders)
+
+@app.route("/orders/<index>")
+def get(index):
+    return render_template("order.html", order=orders[int(index)])
+
